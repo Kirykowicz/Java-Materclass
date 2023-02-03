0ecca92328c999ec918a24e4533c688ba6f06b44
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-03 14:28:44 -0600

    java course with Tim Buchalka

---
+++ section-2/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int passengers = 5;
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+        System.out.println(busTickets);
+
+        int maxValue = 214783647;
+
+        passengers = 6;
+    }
+}
