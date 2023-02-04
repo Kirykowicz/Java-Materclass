1ea41c0913446e7419c2a3a19d754a79c01ca78b
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-03 20:16:36 -0600

    Stataments and whitespace and indentation complete

--- section-4/HelloWorld/src/Challenge.java
+++ section-4/HelloWorld/src/Challenge.java
@@ -1,9 +1,11 @@
 public class Challenge {
     public static void main(String[] args) {
-        double step1 = 20.00;
-        double step2 = 80.00;
-        double step3 = (step1 + step2) * 100.00;
+        double step1 = 20.00d;
+        double step2 = 80.00d;
+        double step3 = (step1 + step2) * 100.00d;
+        System.out.println("My total value is: " + step3);
         double step4 = step3 % 40.00;
+        System.out.println("My remainder is: " + step4);
         boolean step5 = (step4 == 0.00) ? true : false;
         System.out.println("Answer to step 5: " + step5);
         if(!step5){
