64b3e87c70ad31dfa6828e52d692e7ae4257b060
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-05 19:14:31 -0600

    Coding Exercise 8: Teen number checker updated and completed

---
+++ section-5/MethodOverloading/src/TeenNumberChecker.java
@@ -0,0 +1,18 @@
+public class TeenNumberChecker {
+    public static void main(String[] args) {
+
+    }
+    public static boolean hasTeen(int a, int b, int c){
+        if((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isTeen(int num){
+        if(num >= 13 && num <= 19){
+            return true;
+        }
+        return false;
+    }
+}
