77ecc93ceb9b82a2f6958cdbd632d1a230d2a109
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-05 19:08:36 -0600

    Coding challenge 8: has equal sum is completed

---
+++ section-5/MethodOverloading/src/EquallSumChecker.java
@@ -0,0 +1,11 @@
+public class EquallSumChecker {
+    public static void main(String[] args) {
+
+    }
+    public static boolean hasEquallSum(int a, int b, int c){
+        if (a + b == c){
+            return true;
+        }
+        return false;
+    }
+}
