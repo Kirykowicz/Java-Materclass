7f2ecc9ce5e735e1463ed04f6dc83888eb738231
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-16 18:34:07 -0600

    Polymorphism challenge completed

---
+++ section-8/PolymorphismChallengeExercise/src/GasPoweredCar.java
@@ -0,0 +1,28 @@
+public class GasPoweredCar extends Car {
+    private double avgKmPerLitre;
+    private int cylinders;
+
+    public GasPoweredCar(String description) {
+        super(description);
+    }
+
+    public GasPoweredCar(String description, double avgKmPerLitre, int cylinders) {
+        super(description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.cylinders = cylinders;
+    }
+
+    public void test(){
+        System.out.println("Testing if this works");
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Gas -> All %d cylinders are fired up, Ready!%n", cylinders);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Gas -> usage exceeds the average: %.2f %n", avgKmPerLitre);
+    }
+}
