5a02a9513e29b988fa0723d6dda3527a734df7c5
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-18 10:34:26 -0600

    OOP Master Challenge, Bobs Burgers, is completed

--- section-8/OOPMaterChallenge/src/Main.java
+++ section-8/OOPMaterChallenge/src/Main.java
@@ -1,11 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        Item coke = new Item("drink", "coke", 1.50);
-        coke.printItem();
-        coke.setSize("large");
-        coke.printItem();
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("large");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.5);
+//        avocado.printItem();
 
-        Item avocado = new Item("Topping", "avocado", 1.5);
-        avocado.printItem();
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("bacon", "cheese", "mayo");
+//        burger.printItem();
+
+//        MealOrder regularMeal = new MealOrder();
+//        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+//        regularMeal.setDrinkSize("Large");
+//        regularMeal.printItemizedList();
+//
+//        MealOrder secondMeal = new MealOrder("turkey", "7-UP", "chili");
+//        secondMeal.addBurgerToppings("lettuce", "cheese", "mayo");
+//        secondMeal.setDrinkSize("small");
+//        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-UP", "chili");
+        deluxeMeal.addBurgerToppings("Avocado", "bacon", "lettuce", "cheese", "mayo");
+        deluxeMeal.setDrinkSize("small");
+        deluxeMeal.printItemizedList();
     }
 }
