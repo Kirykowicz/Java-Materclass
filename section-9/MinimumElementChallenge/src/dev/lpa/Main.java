a802e59e5e217ff63efff19376f699ce34295097
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-19 11:10:04 -0600

    alternate reverse copy array method created

--- section-9/MinimumElementChallenge/src/dev/lpa/Main.java
+++ section-9/MinimumElementChallenge/src/dev/lpa/Main.java
@@ -6,12 +6,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[] exampleArray = readIntegers();
-        System.out.println(Arrays.toString(exampleArray));
-        reverseArray(exampleArray);
-        System.out.println(Arrays.toString(exampleArray));
-        System.out.println("The min value is: " + findMin(exampleArray));
+//        int[] exampleArray = readIntegers();
+//        System.out.println(Arrays.toString(exampleArray));
+//        reverseArray(exampleArray);
+//        System.out.println(Arrays.toString(exampleArray));
+//        System.out.println("The min value is: " + findMin(exampleArray));
         int[] ex = {1,2,3,4,5};
+//        int[] reversed = reverseCopy(ex);
+        reverseCopyAlternateSolution(ex);
+//        System.out.println(Arrays.toString(reversed));
 
     }
 
@@ -59,5 +62,21 @@ public class Main {
             array[i] = array[maxIndex - i];
             array[maxIndex - i] = temp;
         }
+    }
+
+    private static int[] reverseCopy(int[] array){
+        int[] newArray = Arrays.copyOf(array, array.length);
+        reverseArrayInstructorsSolutions(newArray);
+        return newArray;
+    }
+
+    private static int[] reverseCopyAlternateSolution(int[] array){
+        int[] reversedArray = new int[array.length];
+        int maxIndex = array.length - 1;
+        for(int el : array){
+            reversedArray[maxIndex--] = el;
+            System.out.println(maxIndex);
+        }
+        return reversedArray;
     }
 }
