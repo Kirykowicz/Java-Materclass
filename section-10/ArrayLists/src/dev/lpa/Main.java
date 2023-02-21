b2b3589d1bc800d8a65bb6a082e9e8b606f880a2
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-20 19:15:47 -0600

    List and ArrayList parts 1,2,3 completed

--- section-10/ArrayLists/src/dev/lpa/Main.java
+++ section-10/ArrayLists/src/dev/lpa/Main.java
@@ -7,6 +7,11 @@ record GroceryItem(String name, String type, int count){
     public GroceryItem(String name){
         this(name, "Dairy", 1);
     }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
 }
 
 public class Main {
@@ -19,9 +24,13 @@ public class Main {
         groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
         System.out.println(Arrays.deepToString(groceryArray));
 
+        System.out.println("-".repeat(30));
+
         ArrayList<GroceryItem> groceryList = new ArrayList<>();
         groceryList.add(new GroceryItem("Butter"));
         groceryList.add(new GroceryItem("Yogurt"));
-
+        groceryList.add(1, new GroceryItem("Bananas", "Produce", 3));
+        groceryList.remove(0);
+        System.out.println(groceryList);
     }
 }
