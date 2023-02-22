6ee7ddda7c98676443b6259b7134c357feb7e924
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-21 19:22:52 -0600

    introduction to iterator and ListIterator completed

--- section-10/LinkedList/src/dev/lpa/Main.java
+++ section-10/LinkedList/src/dev/lpa/Main.java
@@ -30,6 +30,8 @@ public class Main {
 
         printItinerary3(placesToVisit);
 
+        testIterator(placesToVisit);
+
     }
 
     public static void addMoreElements(LinkedList<String> list){
@@ -108,5 +110,23 @@ public class Main {
             previousTown = town;
         }
         System.out.println("Trip ends at " + list.getLast());
+    }
+
+    private static void testIterator(LinkedList<String> list){
+
+        var iterator = list.listIterator();
+        while(iterator.hasNext()){
+//            System.out.println(iterator.next());
+            if(iterator.next().equals("Chicago")){
+                iterator.add("Lake Wivenhoe");
+            }
+        }
+        while(iterator.hasPrevious()){
+            System.out.println(iterator.previous());
+        }
+        System.out.println(list);
+
+        var iterator2 = list.listIterator(3);
+        System.out.println(iterator2.next());
     }
 }
