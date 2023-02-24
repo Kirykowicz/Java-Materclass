b1f59ce49acda4329df36f7a81261f079a584adf
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-23 20:17:40 -0600

    abstract classes lectures 1,2 and 3 completed

---
+++ section-11/AbstractClasses/src/dev/test/Main.java
@@ -0,0 +1,13 @@
+package dev.test;
+
+import dev.lpa.Dog;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog newDoggie = new Dog("Lab", "large", 40.00);
+
+//        newDoggie.type; protected field, wont work in this package.
+    }
+}
