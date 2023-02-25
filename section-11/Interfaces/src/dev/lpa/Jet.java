13fce6dde077f95c2279452951e0475ab1864c21
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-25 13:32:14 -0600

    Interfaces part 2 complete

---
+++ section-11/Interfaces/src/dev/lpa/Jet.java
@@ -0,0 +1,21 @@
+package dev.lpa;
+
+public class Jet implements FlightEnabled{
+
+    @Override
+    public void takeOff() {
+        System.out.println(getClass().getSimpleName() + " is taking off");
+    }
+
+    @Override
+    public void land() {
+        System.out.println(getClass().getSimpleName() + " is landing");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getClass().getSimpleName() + " is flying");
+    }
+
+
+}
