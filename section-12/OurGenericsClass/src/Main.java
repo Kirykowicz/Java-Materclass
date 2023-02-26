46273d8a7c37708284fb620b481ff3ad6e2eb74b
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-26 12:02:24 -0600

    Generics, part 1 ,2 3, completed

---
+++ section-12/OurGenericsClass/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("This works");
+        brokenTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordon");
+        melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorne = new Team<>("Hawthorne");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorne.matchResult(fremantle, 1, 0);
+        hawthorne.matchResult(adelaideCrows, 3, 8);;
+
+        System.out.println("Rankings");
+        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
+        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+        System.out.println(fremantle.getName() + ": " + fremantle.ranking());
+        System.out.println(hawthorne.getName() + ": " + hawthorne.ranking());
+
+        System.out.println(adelaideCrows.compareTo(melbourne));
+        System.out.println(adelaideCrows.compareTo(hawthorne));
+
+        ArrayList<Team> teams = new ArrayList<>();
+        teams.add(adelaideCrows);
+        teams.add(fremantle);
+        teams.add(hawthorne);
+
+        System.out.println(teams);
+
+        Collections.sort(teams);
+        System.out.println(teams);
+    }
+}
