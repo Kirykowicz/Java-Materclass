903cd9bb7fb9c7884cb9827f4c2d5e966ad543f2
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-02-28 20:29:27 -0600

    scopping examples, utilizing an inner class

---
+++ section-13/Scope/src/com/kirykowicz/Main.java
@@ -0,0 +1,20 @@
+package com.kirykowicz;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String privateVar = "this is private to main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
+        System.out.println(privateVar);
+
+        scopeInstance.timesTwo();
+
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+
+        innerClass.timesTwo();
+
+    }
+}
