8a2b847c0b24f1890023b0572f1abcbbaa36f75f
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-03-01 20:47:11 -0600

    example using final

---
+++ section-13/Final/src/com/kirykowicz/Main.java
@@ -0,0 +1,20 @@
+package com.kirykowicz;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three = new SomeClass("three");
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+
+        int pw = 674312;
+        Password password = new Password(pw);
+        password.storePassword();
+
+    }
+}
