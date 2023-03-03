d724c195e2acfe1c9068d86724d7ef5ece787510
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-03-02 18:50:21 -0600

    static constructor example, section 14 completed

--- section-13/Final/src/com/kirykowicz/Main.java
+++ section-13/Final/src/com/kirykowicz/Main.java
@@ -4,17 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SomeClass one = new SomeClass("one");
-        SomeClass two = new SomeClass("two");
-        SomeClass three = new SomeClass("three");
+//        SomeClass one = new SomeClass("one");
+//        SomeClass two = new SomeClass("two");
+//        SomeClass three = new SomeClass("three");
+//
+//        System.out.println(one.getInstanceNumber());
+//        System.out.println(two.getInstanceNumber());
+//        System.out.println(three.getInstanceNumber());
+//
+//        int pw = 674312;
+//        Password password = new Password(pw);
+//        password.storePassword();
+//
+//        password.letMeIn(674313);
 
-        System.out.println(one.getInstanceNumber());
-        System.out.println(two.getInstanceNumber());
-        System.out.println(three.getInstanceNumber());
-
-        int pw = 674312;
-        Password password = new Password(pw);
-        password.storePassword();
+        System.out.println("Main method called");
+        SIBTest test = new SIBTest();
+        test.someMethod();
+        System.out.println("Owner is " + SIBTest.owner);
 
     }
 }
