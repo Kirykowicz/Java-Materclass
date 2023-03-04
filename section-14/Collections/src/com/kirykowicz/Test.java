17f2bdbaa2de308fbadca82fa2fd6721339dd350
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-03-03 20:03:50 -0600

    example with collections using Array list

---
+++ section-14/Collections/src/com/kirykowicz/Test.java
@@ -0,0 +1,17 @@
+package com.kirykowicz;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int lastRow = 'A' + (-1);
+        int aChar = 'A';
+        System.out.println((char) 65);
+        System.out.println(aChar);
+
+        System.out.println((int)'R');
+
+        for (char row = 'A'; row < 86; row++){
+            System.out.println(row);
+        }
+    }
+}
