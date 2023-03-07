c6b1ea9198dae943b4c7dec0529bfa704a120f0b
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-03-07 16:23:30 -0600

    Used an example of Comparator<> in the Theatre class

--- section-14/Collections/src/com/kirykowicz/Main.java
+++ section-14/Collections/src/com/kirykowicz/Main.java
@@ -1,19 +1,50 @@
 package com.kirykowicz;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olympian", 8, 12);
-//        theatre.getSeats();
-        if(theatre.reserveSeat("L12")){
-            System.out.println("Please pay");
-        }else {
-            System.out.println("Sorry, seat is taken");
+
+        if(theatre.reserveSeat("D12")){
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Seat already reserved");
         }
-//        if(theatre.reserveSeat("H11")){
-//            System.out.println("Please pay");
-//        }else {
-//            System.out.println("Sorry, seat is taken");
-//        }
+
+        if(theatre.reserveSeat("D12")){
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Seat already reserved");
+        }
+
+        if(theatre.reserveSeat("B13")){
+            System.out.println("Please pay for B13");
+        } else {
+            System.out.println("Seat already reserved");
+        }
+
+        List<Theatre.Seat> reversSeats = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reversSeats);
+        printList(reversSeats);
+
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00", 13.00));
+        priceSeats.add(theatre.new Seat("A00", 13.00));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+        printList(priceSeats);
     }
+
+    public static void printList(List<Theatre.Seat> list){
+        for (Theatre.Seat seat : list){
+            System.out.print(" " + seat.getSeatNumber() + " " + seat.getPrice());
+        }
+        System.out.println();
+        System.out.println("===========================================================");
+    }
+
 }
