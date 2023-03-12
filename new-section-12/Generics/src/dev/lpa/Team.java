b95248d98910b035271d63fe0ded5bbdbfb1c3ac
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-03-12 14:55:31 -0500

    Generics part 2 complete

--- new-section-12/Generics/src/dev/lpa/Team.java
+++ new-section-12/Generics/src/dev/lpa/Team.java
@@ -3,21 +3,21 @@ package dev.lpa;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BaseballTeam {
+public class Team<T> {
 
     private String teamName;
-    private List<BaseballPlayer> teamMembers = new ArrayList<>();
+    private List<T> teamMembers = new ArrayList<>();
     private int totalWins = 0;
     private int totalLosses = 0;
     private int totalTies = 0;
 
-    public BaseballTeam(String teamName) {
+    public Team(String teamName) {
         this.teamName = teamName;
     }
 
-    public void addTeamMember(BaseballPlayer player){
-        if(!teamMembers.contains(player)){
-            teamMembers.add(player);
+    public void addTeamMember(T t){
+        if(!teamMembers.contains(t)){
+            teamMembers.add(t);
         }
     }
 
